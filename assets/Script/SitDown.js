d5a5b4d06ecc864c8e90554e47f109e7c07e7fd6
yanglinbo <yanglinbo@51yche.com>
2016-06-17 12:01:49 +0800

    提交

--- assets/Script/SitDown.js
+++ assets/Script/SitDown.js
@@ -9,7 +9,15 @@ cc.Class({
         t_prefab:{
             default:null,
             type:cc.Prefab
-        }
+        },
+        speed: 0.1,
+        radial_round: {
+            default: null,
+            type: cc.Sprite
+        },
+
+        //倒计时
+        countdown_time:0//执行了多少次
     },
 
     // use this for initialization
@@ -60,17 +68,23 @@ cc.Class({
                 sprite_user.spriteFrame = frame;
             });
         };
-
+        var load_time = function(sprite_process){
+            cc.loader.loadRes("GameMain",cc.SpriteAtlas,function(err,atlas){
+                var frame1 = atlas.getSpriteFrame("game_progress_frame");
+                sprite_process.spriteFrame = frame1;
+            });
+        };
         //坐下
         for(var k in table_data['table_info']){
             var v = table_data['table_info'][k];
-            var node_position = node_table_bg.getChildByName("seat_"+v['seat_number']).getPosition();//获取坐标
-            var node_size = node_table_bg.getChildByName("seat_"+v['seat_number']).getContentSize();//获取node的尺寸
+            var seat_node = node_table_bg.getChildByName("seat_"+v['seat_number']);
+            var node_position = seat_node.getPosition();//获取坐标
+            var node_size = seat_node.getContentSize();//获取node的尺寸
             //名字
-            var label_nick = node_table_bg.getChildByName("seat_"+v['seat_number']).getChildByName("nick").getComponent(cc.Label);
+            var label_nick = seat_node.getChildByName("nick").getComponent(cc.Label);
             label_nick.string = v['user_nick'];
             //带入的筹码
-            var label_chips = node_table_bg.getChildByName("seat_"+v['seat_number']).getChildByName("chips").getComponent(cc.Label);
+            var label_chips = seat_node.getChildByName("chips").getComponent(cc.Label);
             label_chips.string = v['user_chips'];
 
             //头像
@@ -86,10 +100,44 @@ cc.Class({
             var sprite_user = node_user.addComponent(cc.Sprite);
             node_user.parent = node_mark;
             load_avatar(v['user_avatar'],sprite_user);
+
+            //倒计时
+            var node_process = new cc.Node();
+            var sprite_process = node_process.addComponent(cc.Sprite);
+            sprite_process.type = cc.Sprite.Type.FILLED;
+            sprite_process.fillType = cc.Sprite.FillType.RADIAL;
+            sprite_process.fillCenter = new cc.Vec2(0.5,0.5);
+            sprite_process.fillStart = 0;
+            sprite_process.fillRange = 0;
+            node_process.width = node_size['width'];
+            node_process.height = node_size['height'];
+            node_process.scale = 1.3;
+            load_time(sprite_process);
+            node_process.parent = seat_node;
+            var time_interval = 0.1;
+            this.schedule(function(){
+                this._CountDown(sprite_process,20,time_interval);
+            },time_interval);
         }
     },
     // called every frame, uncomment this function to activate update callback
     update: function (dt) {
-        
+
     },
+    //倒计时的进度条
+    _CountDown:function(sprite_process,long_time,time_interval){
+        this.countdown_time++;//已经执行了多少次
+        var cycle_percent = 0.01;//一个周期的部分
+        var cycle_time = 20;//一个周期所需的时间,20秒
+        cycle_time = cycle_time * Math.ceil(parseInt(long_time)/cycle_time); //如果耗时大于一个周期所需的时间，说明用了延时道具
+        var init_speed = cycle_percent / cycle_time;//初始速度
+
+        var fillRange = sprite_process.fillRange;
+        //逆时针转
+        //fillRange = fillRange < range ? fillRange += (this.countdown_time * init_speed): 0;
+        //顺时针转
+        var do_time = time_interval * this.countdown_time;
+        fillRange = do_time < long_time ? fillRange -= (do_time * init_speed):0;
+        sprite_process.fillRange = fillRange;
+    }
 });
