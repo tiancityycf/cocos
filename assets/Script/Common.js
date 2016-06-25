dc5d1c22dff7eff9664ab0d8f4996e105a4daa76
yanglinbo <yanglinbo@51yche.com>
2016-06-25 17:17:27 +0800

    提交

--- assets/Script/Common.js
+++ assets/Script/Common.js
@@ -16,9 +16,6 @@ cc.Class({
     sit_down:function(){
         //加载游戏资源图片
         var me = this;
-        cc.loader.loadRes("GameMain",cc.SpriteAtlas,function(err,atlas){
-            me.GameMain = atlas;
-        });
         cc.loader.loadRes("game_cards",cc.SpriteAtlas,function(err,atlas){
             me.GameCards = atlas;
         });
@@ -59,46 +56,54 @@ cc.Class({
                 sprite_user.spriteFrame = frame;
             });
         };
-        var sb_data = null;//小盲的数据
-        var bb_data = null;//大盲的数据
-        //坐下
-        for(var k in table_data['players']){
-            var v = table_data['players'][k];
-            var seat_node = node_table_bg.getChildByName("seat_"+v['chair_id']);
-            var node_size = seat_node.getContentSize();//获取node的尺寸
-            //名字
-            var label_nick = seat_node.getChildByName("nick").getComponent(cc.Label);
-            label_nick.string = this.getLength(v['nick'])>10?this.cutStr(v['nick'],7):v['nick'];
-            label_nick.fontSize = 20;
-            //带入的筹码
-            var label_chips = seat_node.getChildByName("chips").getComponent(cc.Label);
-            label_chips.string = v['remain_chip']+v['table_chip'];
-            //头像
-            var node_mark = new cc.Node();
-            var mask_user = node_mark.addComponent(cc.Mask);
-            mask_user.type = cc.Mask.ELLIPSE;
-            node_mark.width = node_size['width']-10;
-            node_mark.height = node_size['height']-10;
-            node_mark.setPosition(0.5,0.5);
-            node_mark.parent =  seat_node;
+        cc.loader.loadRes("GameMain",cc.SpriteAtlas,function(err,atlas){
+            me.GameMain = atlas;
+            var sb_data = null;//小盲的数据
+            var bb_data = null;//大盲的数据
+            //坐下
+            for(var k in table_data['players']){
+                var v = table_data['players'][k];
+                var seat_node = node_table_bg.getChildByName("seat_"+v['chair_id']);
+                var node_size = seat_node.getContentSize();//获取node的尺寸
+                //名字
+                var label_nick = seat_node.getChildByName("nick").getComponent(cc.Label);
+                label_nick.string = me.getLength(v['nick'])>10?me.cutStr(v['nick'],7):v['nick'];
+                label_nick.fontSize = 20;
+                //带入的筹码
+                var label_chips = seat_node.getChildByName("chips").getComponent(cc.Label);
+                label_chips.string = v['remain_chip']+v['table_chip'];
+                //头像
+                var node_mark = new cc.Node();
+                var mask_user = node_mark.addComponent(cc.Mask);
+                mask_user.type = cc.Mask.ELLIPSE;
+                node_mark.width = node_size['width']-10;
+                node_mark.height = node_size['height']-10;
+                node_mark.setPosition(0.5,0.5);
+                node_mark.parent =  seat_node;
+                
+                var node_user = new cc.Node();
+                var sprite_user = node_user.addComponent(cc.Sprite);
+                node_user.parent = node_mark;
+                if(v['avatar'] != null && v['avatar']!=""){
+                    node_user.scale = (node_size['width']-10)/120;
+                    load_avatar(v['avatar'],sprite_user);
+                }else{
+                    sprite_user.spriteFrame =  me.GameMain.getSpriteFrame("game_seat_valid");
+                }
 
-            var node_user = new cc.Node();
-            var sprite_user = node_user.addComponent(cc.Sprite);
-            node_user.parent = node_mark;
-            node_user.scale = (node_size['width']-10)/120;
-            load_avatar(v['avatar'],sprite_user);
-            seat_node.getChildByName("game_tip").setLocalZOrder(3);
+                seat_node.getChildByName("game_tip").setLocalZOrder(3);
 
-            if(v['chair_id'] == table_data['start']['sb_chair']){
-                sb_data = v;
-            }else if(v['chair_id'] == table_data['start']['bb_chair']){
-                bb_data = v;
+                if(v['chair_id'] == table_data['start']['sb_chair']){
+                    sb_data = v;
+                }else if(v['chair_id'] == table_data['start']['bb_chair']){
+                    bb_data = v;
+                }
             }
-        }
-        if(parseInt(table_data['table_code'])==0){
-            var label_table_code = node_table_bg.getChildByName("table_code").getComponent(cc.Label);
-            label_table_code.string = "盲注 "+sb_data['table_chip']+"/"+bb_data['table_chip'];
-        }
+            if(parseInt(table_data['table_code'])==0){
+                var label_table_code = node_table_bg.getChildByName("table_code").getComponent(cc.Label);
+                label_table_code.string = "盲注 "+sb_data['table_chip']+"/"+bb_data['table_chip'];
+            }
+        });
     },
     resetSeat:function(){
         var table_data = this.hand_data;
