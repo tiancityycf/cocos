71e37f6769c436503cd0cd210fb845e8f4cd32d4
yanglinbo <yanglinbo@51yche.com>
2016-06-25 10:38:24 +0800

    提交

--- assets/Script/Common.js
+++ assets/Script/Common.js
@@ -67,7 +67,8 @@ cc.Class({
             var node_size = seat_node.getContentSize();//获取node的尺寸
             //名字
             var label_nick = seat_node.getChildByName("nick").getComponent(cc.Label);
-            label_nick.string = v['nick'].length>4 ? v['nick'].substring(0,4)+"...":v['nick'].substring(0,4);
+            label_nick.string = this.getLength(v['nick'])>10?this.cutStr(v['nick'],7):v['nick'];
+            label_nick.fontSize = 20;
             //带入的筹码
             var label_chips = seat_node.getChildByName("chips").getComponent(cc.Label);
             label_chips.string = v['remain_chip']+v['table_chip'];
@@ -119,5 +120,40 @@ cc.Class({
         }else{
             return null;
         }
-    }
+    },
+    //获取字符串长度
+    getLength:function (str) {
+        ///<summary>获得字符串实际长度，中文2，英文1</summary>
+        ///<param name="str">要获得长度的字符串</param>
+        var realLength = 0, len = str.length, charCode = -1;
+        for (var i = 0; i < len; i++) {
+            charCode = str.charCodeAt(i);
+            if (charCode >= 0 && charCode <= 128) realLength += 1;
+            else realLength += 2;
+        }
+        return realLength;
+    },
+    //s截取字符串，中英文都能用@param str：需要截取的字符串@param len: 需要截取的长度
+    cutStr:function (str, len) {
+        var str_length = 0;
+        var str_len = str.length;
+        var str_cut = new String();
+        for (var i = 0; i < str_len; i++) {
+            var a = str.charAt(i);
+            str_length++;
+            if (escape(a).length > 4) {
+                //中文字符的长度经编码之后大于4
+                str_length++;
+            }
+            str_cut = str_cut.concat(a);
+            if (str_length >= len) {
+                str_cut = str_cut.concat("...");
+                return str_cut;
+            }
+        }
+        //如果给定字符串小于指定长度，则返回源字符串；
+        if (str_length < len) {
+            return str;
+        }
+    },
 });
